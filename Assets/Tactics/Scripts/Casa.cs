08c97768e50f0f87dfbe30ef48a498c3b7c2aba8
Thiago Hirata <hirata@devcase.com.br>
2016-10-01 01:32:36 -0300

    Marcar casas possíveis e cancelar o status

---
+++ Assets/Tactics/Scripts/Casa.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Casa : MonoBehaviour {
+    /// <summary>
+    /// Ver MarcarcomoPosicaoPossivel
+    /// </summary>
+    private bool posicaoPossivel;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	/// <summary>
+    /// Esta solução parece que funciona, mas não funciona!
+    /// </summary>
+	void Update () {
+        if(posicaoPossivel)
+        {
+            //uma vez é marcado como posição possível,
+            //fica verificando a cada frame se ela ainda
+            //é uma posição possível - e desabilita quando não
+            //não for mais (ex: o status da state machine não é
+            //mais selecionar movimento)
+            if(!TacticsEngine.main.IsCurrentState(TacticsEngine.STATE_ESCOLHER_MOVIMENTO))
+            {
+                //mudou de estado - não é mais uma posição possível
+                posicaoPossivel = false;
+                transform.FindChild("IndicacaoSelecionavel").gameObject.SetActive(false);
+                GetComponent<MostraIndicacaoOnMouseHover>().enabled = false;
+            }
+        }
+	}
+
+    /// <summary>
+    /// Chamado por TacticsEngine quando determinar que esta casa é
+    /// uma das posições de destino possíveis para o personagem
+    /// </summary>
+    public void MarcarComoPosicaoPossivel()
+    {
+        posicaoPossivel = true;
+        transform.FindChild("IndicacaoSelecionavel").gameObject.SetActive(true);
+        GetComponent<MostraIndicacaoOnMouseHover>().enabled = true;
+    }
+}
--- Assets/Tactics/Scripts/TacticsEngine.cs
+++ Assets/Tactics/Scripts/TacticsEngine.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class TacticsEngine : MonoBehaviour {
     public const string STATE_ESCOLHER_PERSONAGEM = "EscolherPersonagem";
@@ -13,7 +13,7 @@ public class TacticsEngine : MonoBehaviour {
     public static TacticsEngine main;
     private Animator stateMachine;
     public Time timeAtual;
-
+    public IDictionary<Vector3, Casa> tabuleiro;
 
     void Awake()
     {
@@ -27,7 +27,41 @@ public class TacticsEngine : MonoBehaviour {
             Destroy(this);
         }
     }
-    
+
+    void Start()
+    {
+        //busca no cenário todas as casas (objetos que tem o componente Casa)
+        Casa[] casas = FindObjectsOfType<Casa>();
+
+        //Monta o dicionário this.tabuleiro - vai ajudar a achar as casas
+        //a partir de uma posição (ex: quero habilitar a casa na posição
+        //4, 2)
+        //Atenção! Se a casa estiver em uma posição não inteira (ex: 1.0000000001),
+        //vai dar errado!
+        tabuleiro = new Dictionary<Vector3, Casa>();
+        foreach (Casa c in casas)
+        {
+            if (tabuleiro.ContainsKey(c.transform.position))
+            {
+                Debug.LogError("Tem duas casas na posição " + c.transform.position);
+            } else
+            {
+                tabuleiro.Add(c.transform.position, c);
+            }
+                
+        }
+    }
+
+    void Update()
+    {
+        //cancelar operação atual (pode ser, por exemplo, cancelar a seleção de um personagem)
+        if(Input.GetButton("Cancel"))
+        {
+            stateMachine.SetTrigger("Cancelar");
+        }
+
+    }
+
     /// <summary>
     /// Usar este método para determinar qual o estado atual da máquina de estados
     /// </summary>
@@ -49,7 +83,27 @@ public class TacticsEngine : MonoBehaviour {
             stateMachine.SetTrigger("PersonagemSelecionado");
 
             //TODO: calcular as Casas de destino possíveis para o personagem
+            //vou deixar bem simples, por enquanto - pode andar 1 posição
 
+            //lembre que estamos pegando a posição da unity, mas vamos
+            //só considerar o pos.x e o pos.z
+            Vector3 posicaoDoPersonagem = personagem.transform.position;
+            Vector3[] posicoesPossiveis = new Vector3[] { posicaoDoPersonagem + new Vector3(1, 0, 0),
+                posicaoDoPersonagem + new Vector3(-1, 0, 0),
+                posicaoDoPersonagem + new Vector3(0, 0, 1),
+                posicaoDoPersonagem + new Vector3(0, 0, -1)};
+
+            //agora, habilitar as casas das posições possíveis
+            foreach(Vector3 posicaoPossivel in posicoesPossiveis)
+            {
+                if(tabuleiro.ContainsKey(posicaoPossivel))
+                {
+                    Casa casa = tabuleiro[posicaoPossivel];
+                    casa.MarcarComoPosicaoPossivel();
+                }
+            }
+
+            
         }
     }
 }
