a4f420d7083912f9666b8f215fb0b14b01ba07e1
Thiago Hirata <hirata@devcase.com.br>
2016-09-30 21:57:52 -0300

    Tabuleiro com personagem

---
+++ Assets/Tactics/Scripts/Spin.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(Vector3.up, 360f * Time.deltaTime);
+	}
+}
